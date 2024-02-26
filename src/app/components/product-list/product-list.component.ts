cd54babca0ef98a48fd1742e08b0ca61c2bfc477
Lev Nagornov <levnagornov@yahoo.com>
2024-02-26 21:47:09 +0100

    feat: fetch data with product service

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  
+  constructor(private productService: ProductService) { }
 
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProductList().subscribe(
+      products => {this.products = products});
+  }
 }
